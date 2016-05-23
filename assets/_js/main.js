45838baf133e6c32abf889f52975d32617de917b
Kite <ixkaito@gmail.com>
2016-05-23 20:32:03 +0900

    Add sub menus

--- assets/_js/main.js
+++ assets/_js/main.js
@@ -6,8 +6,20 @@ var scrollsmoothly = require('./scrollsmoothly.js');
 
 $(function() {
 
-$('.js-menu-trigger, .js-menu-screen, .js-menu-close, .js-menu .nav a').on('click touchstart', function (e) {
+$('.js-menu-trigger, .js-menu-screen, .js-menu-close').on('click touchstart', function (e) {
   $('.js-menu, .js-menu-screen').toggleClass('is-visible');
+  e.preventDefault();
+});
+
+$('.js-menu .nav a:not(.js-about-menu-trigger)').on('click touchstart', function (e) {
+  $('.js-menu, .js-menu-screen').toggleClass('is-visible');
+});
+
+var text;
+$('.js-about-menu-trigger').attr('href', '').on('click touchstart', function (e) {
+  text = $('i', this).text();
+  $('i', this).text(text == '＋' ? '−' : '＋');
+  $('.js-about-menu').slideToggle();
   e.preventDefault();
 });
 
